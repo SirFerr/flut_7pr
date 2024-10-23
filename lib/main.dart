a9574f626f2cdf47166293e5e5c8ab6e16ebfc44
sirferr <ursusmartimus007@gmail.com>
2024-10-23 10:28:55 +0300

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:dio/dio.dart';
+import 'package:flut_7pr/api.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,6 +23,22 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class RecipeRepository {
+  final Dio _dio = Dio();
+  final String _apiUrl = 'https://api.api-ninjas.com/v1/recipe';
+  final String _apiKey = API;
+
+  Future<Response> fetchRecipes(String query) {
+    return _dio.get(
+      _apiUrl,
+      queryParameters: {'query': query},
+      options: Options(
+        headers: {'X-Api-Key': _apiKey},
+      ),
+    );
+  }
+}
+
 class RecipeListScreen extends StatefulWidget {
   const RecipeListScreen({Key? key}) : super(key: key);
 
@@ -28,34 +47,54 @@ class RecipeListScreen extends StatefulWidget {
 }
 
 class _RecipeListScreenState extends State<RecipeListScreen> {
-  final Dio _dio = Dio();
-  final String _apiUrl = 'https://api.api-ninjas.com/v1/recipe';
-  final String _apiKey = '';
+  final RecipeRepository _repository = RecipeRepository();
 
-  List<dynamic> _recipes = [];
-  bool _isLoading = false;
+  List<dynamic> _recipesFuture = [];
+  List<dynamic> _recipesAsyncAwait = [];
+  bool _isLoadingFuture = false;
+  bool _isLoadingAsyncAwait = false;
 
-  Future<void> fetchRecipes(String query) async {
+  fetchRecipesFuture(String query) {
     setState(() {
-      _isLoading = true;
+      _isLoadingFuture = true;
     });
     try {
-      final response = await _dio.get(
-        _apiUrl,
-        queryParameters: {'query': query},
-        options: Options(
-          headers: {'X-Api-Key': _apiKey},
-        ),
-      );
-      setState(() {
-        _recipes = response.data;
+      print("Начало функции fetchRecipesFuture");
+      final Future<Response> responseFuture = _repository.fetchRecipes(query);
+      responseFuture.then((response) {
+        setState(() {
+          _recipesFuture = response.data;
+        });
+        print("Получено сообщение: ${response.data}");
       });
     } catch (e) {
       print('Ошибка при получении рецептов: $e');
     } finally {
       setState(() {
-        _isLoading = false;
+        _isLoadingFuture = false;
       });
+      print("Завершение функции fetchRecipesFuture");
+    }
+  }
+
+  Future<void> fetchRecipesAsyncAwait(String query) async {
+    setState(() {
+      _isLoadingAsyncAwait = true;
+    });
+    try {
+      print("Начало функции fetchRecipesAsyncAwait");
+      final response = await _repository.fetchRecipes(query);
+      setState(() {
+        _recipesAsyncAwait = response.data;
+      });
+      print("Получено сообщение: ${response.data}");
+    } catch (e) {
+      print('Ошибка при получении рецептов: $e');
+    } finally {
+      setState(() {
+        _isLoadingAsyncAwait = false;
+      });
+      print("Завершение функции fetchRecipesAsyncAwait");
     }
   }
 
@@ -75,27 +114,72 @@ class _RecipeListScreenState extends State<RecipeListScreen> {
                 border: OutlineInputBorder(),
               ),
               onSubmitted: (query) {
-                fetchRecipes(query);
+                fetchRecipesFuture(query); // Future API
+                fetchRecipesAsyncAwait(query); // async-await
               },
             ),
           ),
           Expanded(
-            child: _isLoading
-                ? const Center(child: CircularProgressIndicator())
-                : ListView.builder(
-                    itemCount: _recipes.length,
-                    itemBuilder: (context, index) {
-                      final recipe = _recipes[index];
-                      return Card(
-                        child: ListTile(
-                          title: Text(recipe['title'] ?? 'Без названия'),
-                          subtitle: Text(
-                              recipe['ingredients']?.replaceAll('|', '\n') ??
-                                  'Нет ингредиентов'),
-                        ),
-                      );
-                    },
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      const Text('Future API Рецепты',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold)),
+                      Expanded(
+                        child: _isLoadingFuture
+                            ? const Center(child: CircularProgressIndicator())
+                            : ListView.builder(
+                                itemCount: _recipesFuture.length,
+                                itemBuilder: (context, index) {
+                                  final recipe = _recipesFuture[index];
+                                  return Card(
+                                    child: ListTile(
+                                      title: Text(
+                                          recipe['title'] ?? 'Без названия'),
+                                      subtitle: Text(recipe['ingredients']
+                                              ?.replaceAll('|', '\n') ??
+                                          'Нет ингредиентов'),
+                                    ),
+                                  );
+                                },
+                              ),
+                      ),
+                    ],
                   ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      const Text('Async/Await Рецепты',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold)),
+                      Expanded(
+                        child: _isLoadingAsyncAwait
+                            ? const Center(child: CircularProgressIndicator())
+                            : ListView.builder(
+                                itemCount: _recipesAsyncAwait.length,
+                                itemBuilder: (context, index) {
+                                  final recipe = _recipesAsyncAwait[index];
+                                  return Card(
+                                    child: ListTile(
+                                      title: Text(
+                                          recipe['title'] ?? 'Без названия'),
+                                      subtitle: Text(recipe['ingredients']
+                                              ?.replaceAll('|', '\n') ??
+                                          'Нет ингредиентов'),
+                                    ),
+                                  );
+                                },
+                              ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
